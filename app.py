6ab9ffe3535f005869e7e6c1c152f711af5917ca
Greg Lever <greg.lever@genomicsengland.co.uk>
2018-10-05 09:41:22 +0100

    add get and post methods

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
+# TODO(Greg): Get this running locally (or via docker-compose) in python 3
+# TODO(Greg): Find the python3.6 docker image
 import tornado.ioloop
 import tornado.web
 
 
 class MainHandler(tornado.web.RequestHandler):
+    # def get(self):
+    #     self.write("Hello, world")
+
+    # TODO(Greg): Run this locally
     def get(self):
-        self.write("Hello, world")
+        self.write('<html><body><form action="/myform" method="POST">'
+                   '<input type="text" name="message">'
+                   '<input type="submit" value="Submit">'
+                   '</form></body></html>')
+
+    def post(self):
+        self.set_header("Content-Type", "text/plain")
+
+        self.write("You wrote " + self.get_body_argument("message"))
 
 
 def make_app():
